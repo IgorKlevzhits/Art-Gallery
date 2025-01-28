6d60047c9c92a17b7b6c920d4d69c043fce2e723
IgorKlevzhits <klevzhits94@icloud.com>
2025-01-28 16:44:58 +0300

    Initial Commit

---
+++ Art Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Art Gallery
+//
+//  Created by Игорь Клевжиц on 28.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
